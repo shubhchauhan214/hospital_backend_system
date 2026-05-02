47ca45f5f8dcfd79fe24440239ac1ff60d41a930
shubhchauhan214 <shubhchauhan616@gmail.com>
2026-05-02 17:36:07 +0530

    Doctor Schemas Created

--- app/schemas.py
+++ app/schemas.py
@@ -38,3 +38,33 @@ class PatientResponse(PatientBase):
     class Config:
         from_attributes = True
 
+
+#DOCTOR SCHEMAS
+class DoctorBase(BaseModel):
+    user_id: int
+    department_id: int
+    specialization: str
+    qualification: Optional[str] = None
+    experience_years: int = 0
+    consultation_fee: float = 0
+    is_available: bool = True
+
+class DoctorCreate(DoctorBase):
+    pass 
+
+class DoctorUpdate(BaseModel):
+    department_id: Optional[int] = None
+    specialization: Optional[str] = None
+    qualification: Optional[str] = None
+    experience_years: Optional[int] = None
+    consultation_fee: Optional[float] = None
+    is_available: Optional[bool] = None
+
+class DoctorResponse(DoctorBase):
+    id: int
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
