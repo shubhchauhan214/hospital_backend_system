bf372b51d46fb54862b14006491f9e16d2cb4349
shubhchauhan214 <shubhchauhan616@gmail.com>
2026-06-26 00:07:03 +0530

    Configured and Secured departments using JWT roles

--- app/routes/departments.py
+++ app/routes/departments.py
@@ -3,14 +3,15 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import crud, schemas
+from app.dependencies.roles import admin_required
 
 router = APIRouter(prefix="/departments", tags=["Departments"])
 
 @router.post("/", response_model=schemas.DepartmentResponse, status_code=status.HTTP_201_CREATED)
-def create_department(department: schemas.DepartmentCreate, db: Session = Depends(get_db)):
+def create_department(department: schemas.DepartmentCreate, db: Session = Depends(get_db), current_user=Depends(admin_required)):
     return crud.create_department(db=db, department=department)
 
-@router.get("/", response_model=schemas.DepartmentResponse)
+@router.get("/", response_model=List[schemas.DepartmentResponse])
 def get_departments(skip:int=0, limit:int = 100, db: Session = Depends(get_db)):
     return crud.get_departments(db=db, skip=skip, limit=limit)
 
@@ -20,10 +21,10 @@ def get_department(department_id: int, db: Session=Depends(get_db)):
     return crud.get_department_by_id(db=db, department_id=department_id)
 
 @router.put("/{departent_id}", response_model=schemas.DepartmentResponse)
-def update_department(department_id: int, department: schemas.DepartmentUpdate, db: Session = Depends(get_db)):
+def update_department(department_id: int, department: schemas.DepartmentUpdate, db: Session = Depends(get_db), current_user=Depends(admin_required)):
     return crud.update_department(db=db, department_id = department_id, department_data =department)
 
 
 @router.delete("/{department_id}")
-def delete_department(department_id: int, db: Session=Depends(get_db)):
+def delete_department(department_id: int, db: Session=Depends(get_db), current_user=Depends(admin_required)):
     return crud.delete_department(db=db, department_id=department_id)
